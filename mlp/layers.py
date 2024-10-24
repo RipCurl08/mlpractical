64cfd14f465ed13fca53c9816c865fdb646950c2
RipCurl08 <ripley.curtis@outlook.com>
2024-10-24 19:28:09 +0100

    We good boys!

--- mlp/layers.py
+++ mlp/layers.py
@@ -644,7 +644,7 @@ class DropoutLayer(StochasticLayer):
         assert incl_prob > 0. and incl_prob <= 1.
         self.incl_prob = incl_prob
         self.share_across_batch = share_across_batch
-        self.rng = rng
+        # self.rng = rng
 
     def fprop(self, inputs, stochastic=True):
         """Forward propagates activations through the layer transformation.
